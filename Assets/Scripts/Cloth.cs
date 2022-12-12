67e6c4db2ad17ec5cd804ac93c668a3a87b8c830
Hocky Harijanto <hockharijanto@gmail.com>
2022-12-12 17:59:59 +0700

    add collider and electric fuse scenario - stub

--- Assets/Scripts/Cloth.cs
+++ Assets/Scripts/Cloth.cs
@@ -7,6 +7,7 @@ public class Cloth : MonoBehaviour
     [SerializeField]
     private Material wetMaterial;
     private bool isWet = false;
+    public Fuse MyFuse;
     private void OnTriggerEnter(Collider other) {
     if(other.gameObject.name == "WaterHitbox"){
             Debug.Log("Cloth is wet!");
@@ -19,6 +20,10 @@ public class Cloth : MonoBehaviour
 			Debug.Log("Fire extinguished!");
 			Destroy(other.gameObject);
 		}
+    if(other.gameObject.name == "FireElectrical" && MyFuse.isFuseOff == true && isWet == true) {
+            Debug.Log("Fire extinguished! (Electrical)");
+            Destroy(other.gameObject);
+        }
    }
    
 }
