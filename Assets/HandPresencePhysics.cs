25aae45db960f7b66ec7db5c49d89f46f0617863
Naufal Nursatria <droneshunter14@gmail.com>
2022-12-26 16:42:20 +0700

    Hands V2
    Fixing physics and stuff

---
+++ Assets/HandPresencePhysics.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandPresencePhysics : MonoBehaviour
+{
+    public Transform target;
+    private Rigidbody rb;
+    private Collider[] handColliders;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        handColliders = GetComponentsInChildren<Collider>();
+    }
+
+    public void EnableHandCollider()
+    {
+        foreach (var item in handColliders)
+        {
+            item.enabled = true;
+        }
+    }
+
+    public void DisableHandCollider()
+    {
+        foreach (var item in handColliders)
+        {
+            item.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //hand position
+        rb.velocity = (target.position - transform.position) / Time.fixedDeltaTime;
+
+        //hand rotation
+        Quaternion rotationDifference = target.rotation * Quaternion.Inverse(transform.rotation);
+        rotationDifference.ToAngleAxis(out float angleInDegree, out Vector3 rotationAxis);
+
+        Vector3 rotationDifferenceInDegree = angleInDegree * rotationAxis;
+
+        rb.angularVelocity = (rotationDifferenceInDegree * Mathf.Deg2Rad / Time.fixedDeltaTime);
+    }
+}
