7809e2911b4cab5072a9f94d1a7e16af67331f82
Hocky Harijanto <hockharijanto@gmail.com>
2023-01-20 12:16:09 +0700

    android build & adjustments

---
+++ Assets/Scripts/ExitButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitButton : MonoBehaviour
+{
+    public void ChangeScene(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+
+}
